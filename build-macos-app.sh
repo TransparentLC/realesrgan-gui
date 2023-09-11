fab74b1e133b53c6408aff7f1b60a4dd66e62095
hyrulelinks <134026642+hyrulelinks@users.noreply.github.com>
2023-09-11 03:37:23 -0500

    Update build-macos-app.sh
    Add support for intel chip mac devices

--- build-macos-app.sh
+++ build-macos-app.sh
@@ -1,23 +1,32 @@
-# Step 1
+# Download required files
 ## Download and extract arm64 version from 'realesrgan-ncnn-vulkan'
 curl -L "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-macos.zip" -o realesrgan-ncnn-vulkan-20220424-macos.zip
 unzip -o realesrgan-ncnn-vulkan-20220424-macos.zip
 rm -f realesrgan-ncnn-vulkan-20220424-macos.zip input.jpg input2.jpg onepiece_demo.mp4
-lipo realesrgan-ncnn-vulkan -extract arm64 -output realesrgan-ncnn-vulkan-arm64
-rm -f realesrgan-ncnn-vulkan
-mv realesrgan-ncnn-vulkan-arm64 realesrgan-ncnn-vulkan
-chmod u+x realesrgan-ncnn-vulkan
 
-# Step 2
-## Create and activate virtualenv
-python3 -m venv ./realgui
-source ./realgui/bin/activate
+# Thin fat files to specified architecture
+cpu=$(sysctl -n machdep.cpu.brand_string)
+if [[ $cpu == *"Intel"* ]]; then
+  echo "Intel chip"
+  ditto realesrgan-ncnn-vulkan realesrgan-ncnn-vulkan-x64 --arch x86_64
+  rm -f realesrgan-ncnn-vulkan
+  mv realesrgan-ncnn-vulkan-x64 realesrgan-ncnn-vulkan
+  chmod u+x realesrgan-ncnn-vulkan
+elif [[ $cpu == *"Apple"* ]]; then
+  echo "Apple Silicon"
+  ditto realesrgan-ncnn-vulkan realesrgan-ncnn-vulkan-arm64 --arch arm64
+  rm -f realesrgan-ncnn-vulkan
+  mv realesrgan-ncnn-vulkan-arm64 realesrgan-ncnn-vulkan
+  chmod u+x realesrgan-ncnn-vulkan
+fi
 
-# Step 3
-## Install dependcies
+# Create and activate Python virtualenv
+python3 -m venv 'venv'
+source 'venv/bin/activate'
+
+# Install dependencies
 pip3 install -r requirements.txt
 pip3 install pyinstaller
 
-# Step 4
-## Create macOS App Bundle
-sudo pyinstaller realesrgan-gui-macOS-arm64.spec
+# Build macOS app
+sudo pyinstaller realesrgan-gui-macos.spec
