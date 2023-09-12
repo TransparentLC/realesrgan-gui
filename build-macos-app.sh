329ad9186158642d3f9d112ae7aaa5f9791b399e
hyrulelinks <134026642+hyrulelinks@users.noreply.github.com>
2023-09-11 21:16:56 -0500

    Update build-macos-app.sh

--- build-macos-app.sh
+++ build-macos-app.sh
@@ -3,25 +3,25 @@ git clone https://github.com/TransparentLC/realesrgan-gui.git
 cd realesrgan-gui
 
 # Download required files
-curl -L "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-macos.zip" -o realesrgan-ncnn-vulkan-20220424-macos.zip
-unzip -o realesrgan-ncnn-vulkan-20220424-macos.zip
-rm -f realesrgan-ncnn-vulkan-20220424-macos.zip input.jpg input2.jpg onepiece_demo.mp4
+#curl -L "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-macos.zip" -o realesrgan-ncnn-vulkan-20220424-macos.zip
+#unzip -o realesrgan-ncnn-vulkan-20220424-macos.zip
+#rm -f realesrgan-ncnn-vulkan-20220424-macos.zip input.jpg input2.jpg onepiece_demo.mp4
 
 # Thin fat files to single architecture
-cpu=$(sysctl -n machdep.cpu.brand_string)
-if [[ $cpu == *"Intel"* ]]; then
-  echo "Intel chip"
-  ditto realesrgan-ncnn-vulkan realesrgan-ncnn-vulkan-x64 --arch x86_64
-  rm -f realesrgan-ncnn-vulkan
-  mv realesrgan-ncnn-vulkan-x64 realesrgan-ncnn-vulkan
-  chmod u+x realesrgan-ncnn-vulkan
-elif [[ $cpu == *"Apple"* ]]; then
-  echo "Apple Silicon"
-  ditto realesrgan-ncnn-vulkan realesrgan-ncnn-vulkan-arm64 --arch arm64
-  rm -f realesrgan-ncnn-vulkan
-  mv realesrgan-ncnn-vulkan-arm64 realesrgan-ncnn-vulkan
-  chmod u+x realesrgan-ncnn-vulkan
-fi
+#cpu=$(sysctl -n machdep.cpu.brand_string)
+#if [[ $cpu == *"Intel"* ]]; then
+#  echo "Intel chip"
+#  ditto realesrgan-ncnn-vulkan realesrgan-ncnn-vulkan-x64 --arch x86_64
+#  rm -f realesrgan-ncnn-vulkan
+#  mv realesrgan-ncnn-vulkan-x64 realesrgan-ncnn-vulkan
+#  chmod u+x realesrgan-ncnn-vulkan
+#elif [[ $cpu == *"Apple"* ]]; then
+#  echo "Apple Silicon"
+#  ditto realesrgan-ncnn-vulkan realesrgan-ncnn-vulkan-arm64 --arch arm64
+#  rm -f realesrgan-ncnn-vulkan
+#  mv realesrgan-ncnn-vulkan-arm64 realesrgan-ncnn-vulkan
+#  chmod u+x realesrgan-ncnn-vulkan
+#fi
 
 # Create and activate Python virtualenv
 python_version=$(python -V 2>&1 | cut -d" " -f2 | cut -d. -f1-2)
@@ -32,8 +32,8 @@ else
   echo "Current Python version is $python_version, but 3.11 is required."
   if command -v pyenv >/dev/null 2>&1; then
     pyenv install 3.11
-    pyenv shell 3.11
-    eval "$(pyenv init -)"
+    pyenv global 3.11
+    exec zsh
   else
     echo "pyenv command is not available, please install Python 3.11 manually."
   fi
