30ba093f54f6d91aaee5df7fa80ef49bece03014
hyrulelinks <134026642+hyrulelinks@users.noreply.github.com>
2024-04-08 10:30:19 +0800

    Update macos-build-script.sh

--- macos-build-script.sh
+++ macos-build-script.sh
@@ -1,11 +1,19 @@
+# Ask user whether to use mirror_head
+read -p "Do you want to use mirror_head? (y/n): " use_mirror
+if [ "$use_mirror" = "y" ]; then
+    mirror_head="https://mirror.ghproxy.com/"
+else
+    mirror_head=""
+fi
+
 # Clone repo
 echo "INFO: Clone repo."
-mirror_head="https://mirror.ghproxy.com"
-git clone $mirror_head/https://github.com/TransparentLC/realesrgan-gui.git
+repo_url="https://github.com/TransparentLC/realesrgan-gui.git"
+git clone $mirror_head$repo_url
 cd realesrgan-gui
 
 # Create and activate Python virtual environment
-echo "INFO: 🚀 Checking current Python version..."
+echo "INFO: 🚧 Checking current Python version..."
 python_version=$(python3 -V 2>&1 | cut -d" " -f2 | cut -d"." -f1-2)
 
 if ! which python3 >/dev/null 2>&1; then
@@ -16,18 +24,18 @@ else
     echo "INFO: The 'python3' command found." 
     if [ "$python_version" == "3.12" ]; then
         echo "INFO: ✅ The current Python version is 3.12"
-        echo "INFO: 🚀 Creating Python 3.12 virtual enviroment..."
+        echo "INFO: 🚧 Creating Python 3.12 virtual enviroment..."
         python3 -m venv venv
-        echo "INFO: 🚀 Activating Python virtual enviroment..."
+        echo "INFO: 🚧 Activating Python virtual enviroment..."
         source venv/bin/activate
 
     else
         echo "ERROR: ⛔️ The current Python version is $python_version but 3.12 is required."
-        echo "INFO: 🚀 Installling Python package 'virtualenv'..."
+        echo "INFO: 🚧 Installling Python package 'virtualenv'..."
         pip3 install virtualenv
-        echo "INFO: 🚀 Creating Python 3.12 virtual enviroment..."
+        echo "INFO: 🚧 Creating Python 3.12 virtual enviroment..."
         virtualenv -p python3.12 venv
-        echo "INFO: 🚀 Activating Python virtual enviroment..."
+        echo "INFO: 🚧 Activating Python virtual enviroment..."
         source venv/bin/activate
     fi
 fi
@@ -60,23 +68,23 @@ download_asset() {
 
     # Download the latest release asset
     if [ -n "$asset_url" ]; then
-        echo "INFO: 🚀 Downloading $zip_file..."
-        curl -LO "$mirror_head/$asset_url"
+        echo "INFO: 🚧 Downloading $zip_file..."
+        curl -LO "$mirror_head$asset_url"
         echo "INFO: ✅ Download $zip_file complete."
 
         # Unzip and process the file
-        echo "INFO: 🚀 Extracting and processing files..."
+        echo "INFO: 🚧 Extracting and processing files..."
         unzip -j "$zip_file" "$folder_name/$unzip_target" -d "$output_folder"
 
         # Perform additional actions based on the repo
         if [ "$repo" = "upscayl/upscayl-ncnn" ]; then
-            # Set permissions
+            # Thin file and set permissions
             if [ -f "upscayl-bin" ]; then
                 # Thin file
-                echo "INFO: 🚀 Thin fat files to single architecture..."
+                echo "INFO: 🚧 Thin fat files to single architecture..."
                 arch=$(uname -m)
                 echo "INFO: 💬 System architecture is $arch."
-                echo "INFO: 🚀 Extracting architecture specific libraries..."
+                echo "INFO: 🚧 Extracting architecture specific libraries..."
                 if [ "$arch" = "arm64" ]; then
                     ditto --arch arm64 "$target_file" "$target_file_temp"
                 else
@@ -85,13 +93,13 @@ download_asset() {
                 rm -rf "$target_file"
                 mv "$target_file_temp" "$target_file"
                 # Add execute permission
-                echo "INFO: 🚀 Add execute permission to realesrgan-ncnn-vulkan..."
+                echo "INFO: 🚧 Add execute permission to realesrgan-ncnn-vulkan..."
                 chmod u+x realesrgan-ncnn-vulkan
             fi
         elif [ "$repo" = "nihui/realsr-ncnn-vulkan" ]; then
             # Rename RealSR models
             if [ -f "models/x4.param" ] && [ -f "models/x4.bin" ]; then
-                echo "INFO: 🚀 Rename RealSR models..."
+                echo "INFO: 🚧 Rename RealSR models..."
                 mv models/x4.param models/realsr-x4-realworld.param
                 mv models/x4.bin models/realsr-x4-realworld.bin
             fi
@@ -106,7 +114,7 @@ download_asset() {
                 param_file="models/$model.param"
                 bin_file="models/$model.bin"
                 if [ -f "$param_file" ] && [ -f "$bin_file" ]; then
-                    echo "INFO: 🚀 Removing $model models..."
+                    echo "INFO: 🚧 Removing $model models..."
                     rm -rf "$param_file" "$bin_file"
                 fi
             done
@@ -127,13 +135,13 @@ download_asset "nihui/realsr-ncnn-vulkan" "macos" "models-DF2K/*" "models"
 download_asset "xinntao/Real-ESRGAN" "macos" "*" "models"
 
 # Install dependencies
-echo "INFO: 🚀 Installing requirements..."
+echo "INFO: 🚧 Installing requirements..."
 pip3 install -r requirements.txt
-echo "INFO: 🚀 Installing Python package 'pyinstaller'..."
+echo "INFO: 🚧 Installing Python package 'pyinstaller'..."
 pip install pyinstaller
 
 # Build macOS app
-echo "INFO: 🚀 Packaging macOS app..."
+echo "INFO: 🚧 Packaging macOS app..."
 sudo pyinstaller realesrgan-gui-macos.spec
 
 # Copy built app to Download directory
